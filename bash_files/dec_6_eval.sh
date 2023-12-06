119c0e5eeccd688a448ea430feba6336edf19c24
IsaacJu-debug <ju1@stanford.edu>
2023-12-06 08:41:03 -0800

    bash file for evaluating MAE of property prediction

---
+++ bash_files/dec_6_eval.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Modes
+# 1: Conditional Generation + Eval
+# 2: Conditional Baseline + Eval
+# 3: Unconditional Generation
+# 4: Unconditional Baseline
+# 5: Multi-Objective + Eval
+# 6: Multi-Objective Original + Eval
+
+MODEL="egnn_dynamics"
+DROP_PROB="0.1"
+DATASET_PORTION="0.5"
+PROPERTY="alpha"
+EPOCHS="400"
+TEST_EPOCHS="80"
+MODE="1"
+BATCH_SIZE="168"
+STABILITY_SAMPLES="500"
+
+for GUIDANCE_WEIGHT in 0.1 0.25 0.5 1.0 2; do
+    sbatch bash_files/big_launcher.sh $MODEL $GUIDANCE_WEIGHT $DROP_PROB $DATASET_PORTION $PROPERTY $EPOCHS $TEST_EPOCHS $MODE $BATCH_SIZE $STABILITY_SAMPLES
+done 
+
+MODE="2"
+GUIDANCE_WEIGHT="0"s
+sbatch bash_files/big_launcher.sh $MODEL $GUIDANCE_WEIGHT $DROP_PROB $DATASET_PORTION $PROPERTY $EPOCHS $TEST_EPOCHS $MODE $BATCH_SIZE $STABILITY_SAMPLES
