ed819acf27f8af95c7d21dbd1134f85ec24efe14
Aksh Garg <akshgarg@stanford.edu>
2023-12-10 15:57:59 -0800

    hi

---
+++ bash_files/dec_6_multi_evals.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Modes
+# 1: Conditional Generation + Eval
+# 2: Conditional Baseline + Eval
+# 3: Unconditional Generation
+# 4: Unconditional Baseline
+# 5: Multi-Objective + Eval
+# 6: Multi-Objective Original + Eval
+
+MODEL="egnn_dynamics"
+DROP_PROB="0.1"
+DATASET_PORTION="0.5"
+PROPERTY="alpha"
+EPOCHS="400"
+TEST_EPOCHS="80"
+BATCH_SIZE="168"
+STABILITY_SAMPLES="500"
+EVAL_FLAG="1"
+
+PROPERTY="alpha,mu"
+MODE="5"
+for GUIDANCE_WEIGHT in 0.1 0.25 0.5 1.0 2; do
+    sbatch big_launcher.sh $MODEL $GUIDANCE_WEIGHT $DROP_PROB $DATASET_PORTION $PROPERTY $EPOCHS $TEST_EPOCHS $MODE $BATCH_SIZE $STABILITY_SAMPLES $EVAL_FLAG
+done 
+
+MODE="6"
+GUIDANCE_WEIGHT="0"
+sbatch big_launcher.sh $MODEL $GUIDANCE_WEIGHT $DROP_PROB $DATASET_PORTION $PROPERTY $EPOCHS $TEST_EPOCHS $MODE $BATCH_SIZE $STABILITY_SAMPLES $EVAL_FLAG
